a373d2965dd762384b312a867a1db55ee6ff45eb
moralss <jeramoral@gmail.com>
2018-02-14 09:36:09 +0200

    Created a function that groups arrays together

---
+++ free-code-camp/groupArray/groupingArrays.js
@@ -0,0 +1,23 @@
+// Break it up.
+function chunkArrayInGroups(arr, size) {
+
+
+    var result = [];
+    var temp = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i % size !== size - 1)
+            temp.push(arr[i]);
+        else {
+            temp.push(arr[i]);
+            result.push(temp);
+            temp = [];
+        }
+    }
+
+    if (temp.length !== 0)
+        result.push(temp);
+    return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d", "f", "e"], 1));
