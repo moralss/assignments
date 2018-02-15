217b15209b0468635391fa2e318ffd34d5cb45ff
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-02-15 12:10:52 +0000

    made my find longest word function much clearner

---
+++ free-code-camp/longestWord/longestword.js
@@ -0,0 +1,20 @@
+
+
+        function findLongestWord(string) {
+
+            var longestWord = "";
+
+            var changeIntoAnArray = string.split(' ');
+
+
+            var sortArray = changeIntoAnArray.sort(function (a, b) { return b.length - a.length; });
+            
+            longestWord = sortArray[0];
+
+            return longestWord;
+
+
+
+}
+     
+  
