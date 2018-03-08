3d4d35b9602e77ecd5e117cec5fe6801bef79ab7
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-03-08 14:01:22 +0000

    Checked if every object has its Own Property and has  no falsy values

---
+++ free-code-camp/EverythingBeTrue.js
@@ -0,0 +1,16 @@
+
+
+
+function truthCheck(collection, pre) {
+
+  var counter = 0;
+  for (var i in collection) {
+    
+    if (collection[i].hasOwnProperty(pre) && Boolean(collection[i][pre])) {
+      counter++;
+    }
+  }
+  
+  return counter == collection.length;
+}
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
