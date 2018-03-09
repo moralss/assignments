2e0d487e82e59025466496511db7cb7ab477b6f4
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-03-09 11:21:01 +0000

    created a function to filters element in an array

---
+++ codewars/filterGeese.js
@@ -0,0 +1,16 @@
+function gooseFilter (birds) {
+
+  var geese = ["African", "Roman", "Toulouse", "Pilgrim"];
+
+  return birds.filter( bird => geese.indexOf(bird) === -1 );
+};
+
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]));
+
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]),["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
+
+
+
+
+
+
