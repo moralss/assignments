926e6b2d8747563a16804d6673333dceb6c2ba95
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-03-11 15:05:19 +0000

    encrypting and decrypting

---
+++ codewars/Cipher.js
@@ -0,0 +1,48 @@
+
+
+function encode(str) {
+    var encodedArray = [];
+    var finalArray = [];
+    
+
+    for (var i in str.split('')) {
+        encodedArray.push(str.charCodeAt(i));
+    }
+
+    var newEncodeArray = encodedArray.map(a => a * 6);
+
+    for (var i in newEncodeArray) {
+        finalArray.push(String.fromCharCode(newEncodeArray[i]));
+    }
+
+    return finalArray.join('');
+
+
+}
+
+function decode(str) {
+
+    var encodedArray = [];
+    var finalArray = [];
+    
+
+for (var i in str.split('')) {
+        encodedArray.push(str.charCodeAt(i));
+    }
+
+    var newEncodeArray = encodedArray.map(a => a / 6);
+
+    for (var i in newEncodeArray) {
+        finalArray.push(String.fromCharCode(newEncodeArray[i]));
+    }
+
+    return finalArray.join('');
+    
+}
+
+
+console.log(decode("ưɞʈʈʚÀȊʚʬʈɘÆ"));
+
+// Test.assertEquals(encode("Hello World!"), "ưɞʈʈʚÀȊʚʬʈɘÆ")
+// Test.assertEquals(decode("ưɞʈʈʚÀȊʚʬʈɘÆ"), "Hello World!");
+
