9465da2a3a5bd97d7b35ffb8a9c07e78e2073448
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-03-14 11:20:49 +0000

    modfied my replace function

--- free-code-camp/replace.js
+++ free-code-camp/replace.js
@@ -1,33 +1,20 @@
 function myReplace(str, before, after) {
+    var newString = "";
 
-    var ConvertSTringIntoArray = str.split(' ');
-     var index =  ConvertSTringIntoArray.indexOf(before);
-     
-    var beforeFirstIndex = before.charAt(0);
-    var beforeCapitalizedIndex = before.charAt(0).toUpperCase();
+    if (before.charAt(0) === before.charAt(0).toUpperCase()) {
+        newString = str.replace(before, after.charAt(0).toUpperCase() + after.slice(1));
 
-
-
-
-    if (beforeFirstIndex === beforeCapitalizedIndex) {
-
-        ConvertSTringIntoArray[index] = after.charAt(0).toUpperCase() + after.slice(1);
-
-        finalString = ConvertSTringIntoArray.join(' ');
-        
     }
 
-    else{
-
-         ConvertSTringIntoArray[index] = after;
-
-        var finalString = ConvertSTringIntoArray.join(' ');
+    else {
+        newString = str.replace(before , after);
     }
+    return newString;
 
-    return finalString;
-};
+}
 
-console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
-console.log(myReplace("He is Sleeping on the couch", "Sleeping","sitting"));
+
+ console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
 
 //myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
