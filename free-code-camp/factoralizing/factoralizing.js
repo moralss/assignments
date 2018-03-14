dde167829ba2e24957f6facdc4ceb3a468c2c920
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-03-14 11:22:33 +0000

    made changes made it more readable

--- free-code-camp/factoralizing/factoralizing.js
+++ free-code-camp/factoralizing/factoralizing.js
@@ -1,3 +1,4 @@
+
 var arrNumbersToFactorlize = [];
 var sumTotal = 1;
 
@@ -6,6 +7,8 @@ function factorialize(Number) {
     for (var i = 1; i <= Number; i++) {
         arrNumbersToFactorlize.push(i);
     }
+
+    
     sumTotal = arrNumbersToFactorlize.reduce((a ,b) => a * b );
 
     return sumTotal;
