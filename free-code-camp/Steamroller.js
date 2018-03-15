27276cef158b1c4e4810af3ec8e8002ab087639b
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-03-15 08:35:30 +0000

    removed all the array elements

---
+++ free-code-camp/Steamroller.js
@@ -0,0 +1,23 @@
+function steamrollArray(arr) {
+  var flattenedArray = [];
+
+  var flatter = function (arg) {
+    if (!Array.isArray(arg)) {
+      flattenedArray.push(arg);
+    }
+    else {
+      for (var i in arg) {
+        flatter(arg[i]);
+      }
+    }
+
+  }
+
+arr.forEach(flatter)
+return flattenedArray;
+
+
+return flattenedArray;
+}
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
