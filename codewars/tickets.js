59074d4e6f7747fd3f7d74c0c9b66a63cc1c1111
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-04-18 08:56:38 +0000

    changed my repo name

---
+++ codewars/tickets.js
@@ -0,0 +1,6 @@
+function tickets(array){
+
+};
+
+var myArrayTickets = [25, 25 , 50 , 50];
+console.log(tickets(array));
--- random.js
+++
@@ -1,7 +0,0 @@
-
-function getRandomArbitrary(min, max) {
-    return Math.random() * Math.random(max - min) + Math.random(min);
-}
-
-
-console.log(getRandomArbitrary(1, 9));
