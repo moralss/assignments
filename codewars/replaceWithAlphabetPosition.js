3d8aa3e22bfe8f4f153fb69d686be196097ea678
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-04-23 11:47:24 +0000

    created a function for replacing string letters with  positional numbers in the alphabets

---
+++ codewars/replaceWithAlphabetPosition.js
@@ -0,0 +1,14 @@
+var alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+
+function replaceWithPosition(string) {
+    let positionArray = [];
+    let splitString = string.split('');
+    for (var i in splitString) {
+        positionArray.push(alphabets.indexOf(splitString[i].toLowerCase()));
+    };
+    return positionArray.filter(element => element !== -1).map((a) => a + 1);
+};
+
+let text = "The sunset sets at twelve o' clock.";
+console.log(replaceWithPosition(text));
