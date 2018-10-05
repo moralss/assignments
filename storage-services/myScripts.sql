a1620e9e8474f11857f2bf6c9e25164b6e3df7a6
moralss <jeramoral@gmail.com>
2018-10-05 08:50:59 +0200

    made changes to myScript

--- storage-services/myScripts.sql
+++ storage-services/myScripts.sql
@@ -3,11 +3,11 @@ select * from blocks;
 
 -- 2 
 
-select * from business inner join  location on business.id = location.business_id
+select unit_type.name , unit_type.length , unit_type.height from business inner join  location on business.id = location.business_id
 inner join  block on location.id = block.location_id
 inner join  unit on block.id = unit.block_id 
 inner join  unit_type on  unit.unit_type_id = unit_type.id 
-where block_id = 1;
+where business.id = 1;
 
 -- 3
 select * from unit_type where name = 'garage';
